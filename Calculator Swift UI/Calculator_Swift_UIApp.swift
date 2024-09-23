3ba436c64cb6359ea23c02f5771be7f9f812a7b9
centinna-l <jerryjoy99@gmail.com>
2024-09-22 23:00:50 -0400

    Initial Commit

---
+++ Calculator Swift UI/Calculator_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_Swift_UIApp.swift
+//  Calculator Swift UI
+//
+//  Created by Jerry Joy on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
